d1868268970e8bd751c2d277764967b37845689e
glasfisch3000 <jakob@danckwerts.net>
2024-02-26 14:46:12 +0100

    changed ScreenTimeData.init visibility

--- Sources/screen-time-lib/ScreenTimeData.swift
+++ Sources/screen-time-lib/ScreenTimeData.swift
@@ -2,7 +2,7 @@ public struct ScreenTimeData: Hashable, Codable {
     public var available: Int
     public var used: Int
     
-    init(available: Int = 0, used: Int = 0) {
+    public init(available: Int = 0, used: Int = 0) {
         self.available = available
         self.used = used
     }
