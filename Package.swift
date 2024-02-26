d7d1e5c0b2a194b9e29352aba263218874a4696a
glasfisch3000 <jakob@danckwerts.net>
2024-02-26 14:36:57 +0100

    removed tests

--- Package.swift
+++ Package.swift
@@ -6,18 +6,14 @@ import PackageDescription
 let package = Package(
     name: "screen-time-lib",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "screen-time-lib",
-            targets: ["screen-time-lib"]),
+            targets: ["screen-time-lib"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "screen-time-lib"),
-        .testTarget(
-            name: "screen-time-libTests",
-            dependencies: ["screen-time-lib"]),
+            name: "screen-time-lib"
+        )
     ]
 )
